b958beb5b9b5ddc82456e497e3e6957536020aa8
AKASHPRABHU <108968305+PRABHUAKASH@users.noreply.github.com>
2022-07-19 20:38:04 +0530

    Add files via upload

---
+++ assresume.js
@@ -0,0 +1,9 @@
+var resume = {
+    "Name" : "AKASHPRABHU";
+    "Age"  : "23";
+    "Qualification" : "B.E.Mechanical Engineering";
+    "Y.O.P" : "2020";
+    "Skills" : "Full Stack Web Developer";
+    "Languages" : "Tamil ","English";
+    "Interests" : "Learning Something New";
+ }
